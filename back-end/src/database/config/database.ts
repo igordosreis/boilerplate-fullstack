fc8de1ec1fd07024b26bc828391947a42cb5af97
Igor dos Reis <igorreis@gmail.com>
2023-08-29 18:15:55 -0300

    build: add back-end folders and example files

--- back-end/src/database/config/database.ts
+++ back-end/src/database/config/database.ts
@@ -4,7 +4,7 @@ import 'dotenv/config';
 const config: Options = {
   username: process.env.DB_USER || 'root',
   password: process.env.DB_PASS || '123456',
-  database: process.env.DB_NAME || 'db-name',
+  database: process.env.DB_NAME || 'db_name',
   host: process.env.DB_HOST || 'localhost',
   port: Number(process.env.DB_PORT) || 3306,
   dialect: 'mysql',
